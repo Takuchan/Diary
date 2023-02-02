297c0711d34373953a62a23bf9c35f4264acc789
Takuchan <takuchanapp@gmail.com>
2023-02-02 14:10:30 +0900

    Initial Commit

---
+++ Diary/DiaryApp.swift
@@ -0,0 +1,17 @@
+//
+//  DiaryApp.swift
+//  Diary
+//
+//  Created by 松村拓洋 on 2023/02/02.
+//
+
+import SwiftUI
+
+@main
+struct DiaryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
